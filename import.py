ccd29b5dcc78468ae0c238f35fcbaa0e028bfcbd
jonolovelock <jonolovelock@gmail.com>
2019-09-01 11:21:14 +1000

    app refactored for sql execute and import.py file working

--- import.py
+++ import.py
@@ -4,16 +4,16 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv("postgresql://postgres:jono@localhost:5432/bookshop"))
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
-    f = open("flights.csv")
+    f = open("books.csv")
     reader = csv.reader(f)
-    for origin, destination, duration in reader:
-        db.execute("INSERT INTO flights (origin, destination, duration) VALUES (:origin, :destination, :duration)",
-                    {"origin": origin, "destination": destination, "duration": duration})
-        print(f"Added flight from {origin} to {destination} lasting {duration} minutes.")
+    for isbn, title, author, year in reader:
+        db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+                    {"isbn": isbn, "title": title, "author": author, "year": year})
+        print(f"The {title} written by {author} in the year {year}  has been added to the db.")
     db.commit()
 
 if __name__ == "__main__":
