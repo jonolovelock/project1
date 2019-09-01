f16246cf6439c5848431ba4c3b1d6f696ba32097
jonolovelock <jonolovelock@gmail.com>
2019-09-01 14:57:40 +1000

    minor format changes only

--- application.py
+++ application.py
@@ -29,15 +29,12 @@ def books():
     if request.method == "POST":
         if request.form.get("isbn"):
             formIsbn = "%" + request.form.get("isbn") + "%"
-
             books = db.execute("SELECT * from books WHERE isbn LIKE :isbn", {"isbn": formIsbn})
         if request.form.get("title"):
             formTitle = "%" + request.form.get("title") + "%"
-
             books = db.execute("SELECT * from books WHERE title LIKE :title", {"title": formTitle})
         if request.form.get("author"):
             formAuthor = "%" + request.form.get("author") + "%"
-
             books = db.execute("SELECT * from books WHERE author LIKE :author", {"author": formAuthor})
         return render_template("books.html", books=books)
     return render_template("books.html")
@@ -65,5 +62,4 @@ def login():
 
 @app.route("/")
 def index():
-
     return render_template("index.html")
